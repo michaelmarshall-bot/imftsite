6a39856c73da965b83f8d45ab1418bdf6d6b2e75
Michael Marshall <michaelsmarshall2@gmail.com>
2022-10-04 11:56:34 -0400

    [History Page] JS Date adjustment + Drone Shot
    Corrected the JS for the mobile version of the date in the page body and updated the lead photo to the newly received drone shot of the field.

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 document.getElementById("currentYearHist").innerHTML = new Date().getFullYear();
+document.getElementById("currentYearHistMob").innerHTML = new Date().getFullYear();
